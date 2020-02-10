d9fefabd9e66db28fcfb0df4b03a5d2f551772b4
Tung Huynh <htqtung@gmail.com>
2020-02-10 17:48:07 +0200

    minor fixes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,25 +1,37 @@
 // @flow
 import React from "react";
 import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
 
 import Copyright from "./Copyright";
 
+const useStyles = makeStyles(theme => ({
+  footer: {
+    width: "100%",
+    marginBottom: theme.spacing(4)
+  }
+}));
+
 // Just a simple footer
 const Footer = () => {
+  const classes = useStyles();
   return (
     <React.Fragment>
-      <Typography
-        variant="subtitle1"
-        align="center"
-        color="textSecondary"
-        component="p"
-      >
-        Have a nice day!{" "}
-        <span role="img" aria-label="love">
-          &#10084;&#65039;
-        </span>
-      </Typography>
-      <Copyright name="Tung Huynh" />
+      <Container className={classes.footer} maxWidth="md">
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="textSecondary"
+          component="p"
+        >
+          Have a nice day!{" "}
+          <span role="img" aria-label="love">
+            &#10084;&#65039;
+          </span>
+        </Typography>
+        <Copyright name="Tung Huynh" />
+      </Container>
     </React.Fragment>
   );
 };
--- src/components/MapView/index.js
+++ src/components/MapView/index.js
@@ -27,7 +27,7 @@ const MapView = ({ restaurants }: Props) => {
         restaurants={restaurants}
         googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${key}`}
         loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `400px` }} />}
+        containerElement={<div style={{ height: `100%` }} />}
         mapElement={<div style={{ height: `100%` }} />}
       />
     </Container>
